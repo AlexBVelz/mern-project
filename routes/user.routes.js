6d53a49208128d44d5262d47cc67aefd268a9fff
HyunfixFR <velzogfr@gmail.com>
2025-02-11 17:09:56 +0100

    feat(mern-project): add route login, logout

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,9 @@ const usercontroller = require('../controllers/user.controller');
 
 // auth
 router.post("/register", authController.signUp);
+router.post('/login', authController.signIn);
+router.get('/logout', authController.logout);
+
 
 
 //user display : 'block'
